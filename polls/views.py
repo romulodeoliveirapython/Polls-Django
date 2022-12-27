f487091328b484c5ab527bf877ec8065654a7874
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2022-12-26 23:47:01 -0300

    pollsatt01

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse('Olá, Mundo. Você está no index do Polls!')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse('Você está olhando a questão %s.' % question_id)
+
+def results(request, question_id):
+    response = 'Você está olhando os resultados da questão %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Você está votando na questão %s.' % question_id)
