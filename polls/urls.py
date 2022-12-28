e7f8065e322b44ac9947f75de4e1c9a2d888ea40
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2022-12-27 21:33:59 -0300

    pollsatt02

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from polls import views
 
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:question_id>', views.detail, name = 'detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from polls.models import Question
 
@@ -9,7 +9,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse('Você está olhando a questão %s.' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = 'Você está olhando os resultados da questão %s.'
