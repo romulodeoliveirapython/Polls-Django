b4c64f9cc3aae0c0a94b7012860bee9f748b087b
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2022-12-28 21:52:19 -0300

    pollsatt04

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,4 @@
-import definitions
+from .definitions import chave_secreta
 
 """
 Django settings for mysite project.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = definitions.chave_secreta
+SECRET_KEY = chave_secreta
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
