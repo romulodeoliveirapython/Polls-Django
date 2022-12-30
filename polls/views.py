41bf46065278d29a862e493ec838ac69745e02b1
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2022-12-30 10:18:48 -0300

    edição das views vote e results

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from polls.models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from polls.models import Choice, Question
+from django.urls import reverse
+
 
 
 def index(request):
@@ -13,8 +15,20 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = 'Você está olhando os resultados da questão %s.'
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse('Você está votando na questão %s.' % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Exiba novamente o formulário de votação da pergunta.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'Você não selecionou uma opção.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
