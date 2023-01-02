f1608a0e0de147e023d080a3879b92d6e888581c
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-02 01:44:52 -0300

    Correção no código do ResultsView

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
     path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
-    path('<int:pk>/results/', views.ResultsViews.as_view(), name = 'results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name = 'results'),
     path('<int:question_id>/vote/', views.vote, name = 'vote'),
 ]
