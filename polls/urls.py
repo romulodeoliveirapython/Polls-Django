4dba6810dc42a39df1d3bd9c3f2b3247245ee371
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-01-01 22:13:23 -0300

    Alterando as URLs index, detail, results e vote

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from polls import views
+from . import views
 
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('<int:question_id>', views.detail, name = 'detail'),
-    path('<int:question_id>/results/', views.results, name = 'results'),
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.DetailView.as_view(), name = 'detail'),
+    path('<int:pk>/results/', views.ResultsViews.as_view(), name = 'results'),
     path('<int:question_id>/vote/', views.vote, name = 'vote'),
 ]
